c8d878e5d5e89f32d6701ceda50a9c68069b8a2e
joshua-H05 <joshuayuanhu@gmail.com>
2022-04-17 18:29:16 +0200

    updated "list_sorting_experiments"

--- chapter 4/list_sorting_experiments.py
+++ chapter 4/list_sorting_experiments.py
@@ -1,12 +1,12 @@
-"""#inspired by the note about sorting lists with capital letters on page 45
-fruits=["Bananas", "apples", "Pears", "strawberries", "Oranges"]#original
+# inspired by the note about sorting lists with capital letters on page 45
+fruits = ["Bananas", "apples", "Pears", "strawberries", "Oranges"]#original
 fruits_string= f"{fruits}"#convert the list into a string
 fruits_lower=fruits_string.lower()#apply the .lower() function to the string
 fruits=[]#make fruits an empty list so I can add fruits_lower back into the list
 fruits.append(fruits_lower) #add fruits_lower into the list--> the list should now have lower case values only
 fruits=fruits[0] #seems that I've created a list inside the empty list. Why is this? To solve this, I redefined fruits using the index 0
 print(fruits)#now I can no longer apply the sort function. Is it no longer a list?
-"""
+
 
 
 # Attempt nr.2
@@ -16,13 +16,15 @@ fruits_string= " ".join(fruits).lower()  # join(): for every fruit in fruits, jo
 # applying 2 functions in a line--> method chaining--> saves extra line of code--> more efficient for python to do
 # all of this in one step
 print(fruits_string)
-fruits_lower=list(fruits_string.lower())
+fruits_lower = " ".split(fruits_string)
+"""fruits_lower=list(fruits_string.lower())"""  # This doesn't work because it converts each character
+# into an item in the list
 fruits_lower.sort()
 print(fruits_lower)
 
 
-"""new_fruits= []
+new_fruits= []
 for fruit in fruits:
     new_fruits.append(fruit.lower())
 new_fruits.sort()
-print(new_fruits)"""
+print(new_fruits)
