27ceff52c12dd9bc1cee7345c947d12e780d2b52
joshua-H05 <joshuayuanhu@gmail.com>
2022-04-20 01:32:28 +0200

    updates

---
+++ chapter 6/favorite_numbers.py
@@ -0,0 +1,12 @@
+fav_numbers = {
+    "Tom": 2,
+    "Alice": 7,
+    "Joshua": 9,
+    "Jane": 12,
+    "Max": 6
+}
+print(f"Tom's favorite number is {fav_numbers['Tom']}")
+print(f"Alice's's favorite number is {fav_numbers['Alice']}")
+print(f"Joshua's favorite number is {fav_numbers['Joshua']}")
+print(f"Jane's favorite number is {fav_numbers['Jane']}")
+print(f"Max's favorite number is {fav_numbers['Max']}")
---
+++ chapter 6/glossary.py
@@ -0,0 +1,10 @@
+definitions = {
+    "slicing": "a feature that enables accessing parts of sequences like strings, tuples, and lists",
+    ".join()": "an in-built method used to join an iterable's elements, "
+               "separated by a string separator, which is specified by you",
+    ".split()": "splits a string into a list based on a character specified by you",
+}
+print(f"slicing: {definitions['slicing']}\n")
+print(f".join(): {definitions['.join()']}\n")
+print(f".split(): {definitions['.split()']}\n")
+
