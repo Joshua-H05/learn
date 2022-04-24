e61a4fb11165b1fa82ad1b5070160ca3559b7583
joshua-H05 <joshuayuanhu@gmail.com>
2022-04-24 16:30:19 +0200

    updates

--- chapter 6/favorite_numbers.py
+++ chapter 6/favorite_numbers.py
@@ -1,16 +1,19 @@
 fav_numbers = {
-    "Tom": 2,
-    "Alice": 7,
-    "Joshua": 9,
-    "Jane": 12,
-    "Max": 6
+    "Tom": [2, 3,10],
+    "Alice": [7],
+    "Joshua": [9,12],
+    "Jane": [12],
+    "Max":[6,8]
 }
-for name, number in fav_numbers.items():
-    print(f"{name}'s favorite number is {number}\n")
-for number in fav_numbers.values():
-    print (number)
+for name, persons_fav_numbers in fav_numbers.items():
+    if len(persons_fav_numbers) == 1:
+        print(f"{name}'s favorite number is {persons_fav_numbers}\n")
+    elif len(persons_fav_numbers) > 1:
+        print(f"{name}'s favorite numbers are {'and '.join(str(persons_fav_numbers).strip())}\n")
 
-your_fav_number = {}
+
+
+"""your_fav_number = {}
 numbers = input("What's your favorite number?")
 your_fav_number["your favorite number"] = numbers
 friends_numbers = input("What's your friend's favorite number?")
@@ -19,4 +22,4 @@ for person, number in your_fav_number.items():
     print(f"{person} is {number}\n")
 for person in sorted(your_fav_number.keys()):
     number = your_fav_number[person]
-    print(f"{person},{number}")
+    print(f"{person},{number}")"""
---
+++ chapter 6/polling.py
@@ -0,0 +1,15 @@
+favorite_languages = {
+    "Jen": "python",
+    "Sarah": "c",
+    "Edward": "ruby",
+    "Phil": "python"
+}
+should_take_poll = ["Jen", "Edward", "Phil", "Tom"]
+for person in favorite_languages.keys():
+    print (f"Hi, {person}")
+    if person in should_take_poll:
+        language = favorite_languages[person]
+        print(f"{person}, I see you like {language}!\n")
+for person in should_take_poll:
+    if person not in favorite_languages.keys():
+        print(f"{person}, you should take the poll!")
