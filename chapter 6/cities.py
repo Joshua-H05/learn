c7b223333d989971f3c9010e6b45f6836a31dbda
joshua-H05 <joshuayuanhu@gmail.com>
2022-04-25 18:41:08 +0200

    updates

--- chapter 6/cities.py
+++ chapter 6/cities.py
@@ -1,20 +1,23 @@
+# Dictionary nested in another dictionary: The main dictionary uses cities as keys and information on them as values
+# The nested dictionaries use types of info as keys and the info as values
 cities = {
     "London": {"continent": "Europe", "population": "9 million", "language": "English"},
     "Paris": {"continent": "Europe", "population": "2.2 million", "language": "French"},
     "Melbourne": {"continent": "Oceana", "population": "5 million", "language": "English"},
 }
-# Print info on all 3 cities using a fstring
+# iterates around each city in the main dictionary and prints information on them using the nested dictionaries
 for city, info in cities.items():
     print(f"Here's some information about {city}: It is in {info['continent']}, "
           f"it has a population of roughly {info['population']} and the official language there is {info['language']}.")
-    # creates a list of all the cities by appending all the keys of the dictionaries into a list
+# An empty list of cities
 list_of_cities = []
+# Iterates around the keys in the main dictionary, taking each key and appending it into the list of cities
 for city in cities.keys():
     list_of_cities.append(city)
-print(f"{list_of_cities}")
 # Asks the user for his/ her favorite city
 favorite_city = input("What is your favorite city?")
-
+# Checks if the user's favorite city is in the list of cities/ in the dictionary
+# If it is, information on the city is printed. If not, the program lets the user know
 if favorite_city in list_of_cities:
     print(f"Here is some information on {favorite_city}: "
             f"It is in {cities[favorite_city]['continent']}, "
