7ac825b29b242dba72d15b45f25d29b97231670d
joshua-H05 <joshuayuanhu@gmail.com>
2022-04-28 21:06:59 +0200

    updates

--- chapter 7/multiples_of_ten.py
+++ chapter 7/multiples_of_ten.py
@@ -54,3 +54,18 @@ while True:
         print(f"{number} is a multiple of 10")
     else:
         print(f"{number} is not a multiple of 10")
+
+
+prompt = "Give me a number and I'll tell you if it's a multiple of 10: "
+prompt += "\n press q and then enter to quit "
+number = ""
+while number != "q":
+    number = input(prompt)
+    if number == "q":
+        continue
+    number = int(number)
+    if number % 10 == 0:
+            print(f"{number} is a multiple of 10")
+    else:
+            print(f"{number} is not a multiple of 10")
+
