df71e875e2c259784110aa5d22323dc9c5994b4d
joshua-H05 <joshuayuanhu@gmail.com>
2022-04-28 21:16:37 +0200

    updates

--- chapter 7/multiples_of_ten.py
+++ chapter 7/multiples_of_ten.py
@@ -16,10 +16,11 @@ while number != "q":
 # Solution:
 prompt = "Give me a number and I'll tell you if it's a multiple of 10: "
 prompt += "\n press q and then enter to quit "
-number = ""
+number = ""  # Sets number as an empty string so that the program can be run the fist time around
 while number != "q":
-    number = input(prompt)
-    if number != "q":
+    number = input(prompt)  #
+    if number != "q":  # If number is "q", this round of the program stops and python goes back to
+        # the line "while number != "q":" and now that number = "q" the conditional testing fails and the code stops
         number = int(number)
         if number % 10 == 0:
             print(f"{number} is a multiple of 10")
@@ -29,11 +30,11 @@ while number != "q":
 
 prompt = "Give me a number and I'll tell you if it's a multiple of 10: "
 prompt += "\n press q and then enter to quit "
-active = True
+active = True  # Flag: sets the variable "active" to equal True
 while active:
     number = input(prompt)
     if number == "q":
-        active = False
+        active = False  # If number =="q", the value of "active" is changed to equal False and the program stops running
     else:
         number = int(number)
         if number % 10 == 0:
@@ -47,7 +48,7 @@ prompt += "\n press q and then enter to quit "
 number = ""
 while True:
     number = input(prompt)
-    if number == "q":
+    if number == "q":  # if the input is "q", stop the program
         break
     number = int(number)
     if number % 10 == 0:
@@ -60,10 +61,10 @@ prompt = "Give me a number and I'll tell you if it's a multiple of 10: "
 prompt += "\n press q and then enter to quit "
 number = ""
 while number != "q":
-    number = input(prompt)
+    number = input(prompt)  # asks for either a number or "q"
     if number == "q":
-        continue
-    number = int(number)
+        continue  # if the input is q, go back to the beginning of the program and test the condition "number != "q" "
+    number = int(number)  # if not, the program continues
     if number % 10 == 0:
             print(f"{number} is a multiple of 10")
     else:
