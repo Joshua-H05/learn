b4f5ccd30f66d1a5c03b937ba29ee93c591c2a4b
joshua-H05 <joshuayuanhu@gmail.com>
2022-05-01 20:58:30 +0200

    updates

--- chapter 8/t_shirt.py
+++ chapter 8/t_shirt.py
@@ -1,4 +1,5 @@
 def make_shirt(size="L", message="I love Python"):
+    """Prints the requested size of the T-shirt and the message that should be printed on it"""
     print(f'You would like a t-shirt in size {size} with the message "{message}" printed on it.')
 
 
