9efcd03b41dad66e15280d7217e55a113164c8a9
joshua-H05 <joshuayuanhu@gmail.com>
2022-05-06 12:57:59 +0200

    updates

---
+++ chapter 8/city_names.py
@@ -0,0 +1,13 @@
+def format_location(city, country):
+    location = f"{city},{country}"
+    return location
+while True:
+    print("This program will as you where you're from. Press q at any time to quit")
+    country = input("What country do you live in? ")
+    if country == "q":
+        break
+    city = input("Which city do you live in? ")
+    if city == "q":
+        break
+    location = format_location(city, country)
+    print(f"You are from {location}")
