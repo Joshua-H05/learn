946d81e2f9f68e364c6d4fdf5ee0cb18daefa6a9
joshua-H05 <joshuayuanhu@gmail.com>
2022-05-10 22:05:31 +0200

    updates

---
+++ chapter 8/profile_maker_function.py
@@ -0,0 +1,4 @@
+def profile_maker(first_name, last_name, **info):
+    info["first name"] = first_name
+    info["last name"] = last_name
+    return info
---
+++ chapter 8/user_profile_using_imported_functions.py
@@ -0,0 +1,10 @@
+import profile_maker_function
+from profile_maker_function import profile_maker
+from profile_maker_function import profile_maker as pm
+import profile_maker_function as pmf
+from profile_maker_function import *
+print(profile_maker_function.profile_maker("Joshua", "Hu", age=16))
+print(profile_maker("Joshua", "Hu", age=16))
+print(pm("Joshua", "Hu", age=16))
+print(pmf.profile_maker("Joshua", "Hu", age=16))
+print(profile_maker("Joshua", "Hu", age=16))
