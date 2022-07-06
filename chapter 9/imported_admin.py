27d77132f207ea3a3c3bdbdf218af30c35aeaf0b
joshua-H05 <joshuayuanhu@gmail.com>
2022-07-06 14:18:00 +0200

    changes

--- chapter 9/imported_admin.py
+++ chapter 9/imported_admin.py
@@ -1,7 +1,6 @@
-import users
 import admin
 
-user_1 = users.User("Jack", "Ma", 57, "Entrepreneur")
+user_1 = admin.User("Jack", "Ma", 57, "Entrepreneur")
 user_1.greet_user()
 admin_1 = admin.Admin("Joshua", "Hu", 16, "Student")
 admin_1.greet_user()
@@ -10,7 +9,7 @@ admin_1.increment_login_attempts()
 admin_1.total_login_attempts()
 admin_1.login_reset()
 admin_1.total_login_attempts()
-joshua_privileges = users.Privileges(("delete posts", "block users", "unblock users"))
+joshua_privileges = admin.Privileges(("delete posts", "block users", "unblock users"))
 # This instance takes a tuple, so don't forget to put all the strings (privileges) into a tuple
 joshua_privileges.show_privileges()
 admin_1.privileges = joshua_privileges  # override the original instance of Privileges that was instantiated by default
