7001554751e2f31358576e4c35f065a71986564f
joshua-H05 <joshuayuanhu@gmail.com>
2022-07-09 15:27:45 +0200

    updates

--- chapter 10/favorite_number.py
+++ chapter 10/favorite_number.py
@@ -1,6 +1,26 @@
 import json
-from store_favorite_number import get_new_username_and_fav_number
+
 filename = "usernames_and_favorite_numbers.json"
+
+
+def get_new_username_and_fav_number():
+    user_info = {}
+    while True:
+        username = str(input("What's your name? Press q to quit ")).title()
+        if username.lower() == "q":
+            print("Thanks! Your information has been stored. ")
+            break
+        else:
+            try:
+                fav_number = int(input("What is your favorite number? "))
+            except ValueError:
+                print("You can only type in a number! ")
+                fav_number = int(input("Please type in a number: "))
+                user_info[username] = fav_number
+            else:
+                user_info[username] = fav_number
+    with open(filename, "w") as f:
+        json.dump(user_info, f)
 
 
 def display_favorite_number(name):
